ca0b44bfe6d8397048d8bf1994127c67a2863a2d
saim-x <x_aci@hotmail.com>
2024-09-28 22:45:32 +0500

    eslint issuesss

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import AnimatedBackground from '@/components/AnimatedBackground';
 const inter = Inter({ subsets: ['latin'] });
 const firaCode = Fira_Code({ subsets: ['latin'] });
 
-// Add this at the top of your file or in a separate icons file
-const WorkIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-  </svg>
-);
+// // Add this at the top of your file or in a separate icons file
+// const WorkIcon = () => (
+//   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+//   </svg>
+// );
 
 export default function Home() {
   const [currentSection, setCurrentSection] = useState(0);
